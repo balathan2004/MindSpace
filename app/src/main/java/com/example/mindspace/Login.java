7c6aac35d4a9deaf7f3b8db958eda6e2a58db273
light <balathan2004@gmail.com>
2025-10-31 21:14:48 +0530

    input validate progress

--- app/src/main/java/com/example/mindspace/Login.java
+++ app/src/main/java/com/example/mindspace/Login.java
@@ -3,6 +3,9 @@ package com.example.mindspace;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
 
@@ -13,6 +16,36 @@ public class Login extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        Button btn = findViewById(R.id.submit);
+
+        EditText email = findViewById(R.id.email);
+        EditText password = findViewById(R.id.password);
+
+
+        btn.setOnClickListener(e -> {
+            String emailValue = validateInput(email,6);
+            Log.i("Login", "email:" + emailValue);
+            Log.i("Login", password.getText().toString());
+
+        });
+
+
+    }
+
+    String validateInput(EditText input,int minLenght) {
+
+        String value = input.getText().toString();
+
+        if (value.isEmpty()) {
+            input.setError("Input is empty");
+            return null;
+        }
+        if(value.length()<minLenght){
+            input.setError("Input should be "+minLenght+"chars");
+            return null;
+        }
+        return value;
 
     }
 
