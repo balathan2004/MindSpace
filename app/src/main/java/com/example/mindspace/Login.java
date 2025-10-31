ed4f96ecfa71c2c1f8099416cf04fd04529d8c6f
light <balathan2004@gmail.com>
2025-10-31 09:24:08 +0530

    added login page

---
+++ app/src/main/java/com/example/mindspace/Login.java
@@ -0,0 +1,20 @@
+package com.example.mindspace;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+    }
+
+
+}
--- app/src/main/java/com/example/mindspace/MainActivity.java
+++ app/src/main/java/com/example/mindspace/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.mindspace;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+
 public class MainActivity extends AppCompatActivity {
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
-        setContentView(R.layout.login);
+      
+        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(MainActivity.this, Login.class);
+        startActivity(intent);
+
     }
 
 }
