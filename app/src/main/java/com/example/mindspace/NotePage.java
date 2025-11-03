69fb0c3d47e68d817473f43520a9b230fd11f546
light <balathan2004@gmail.com>
2025-11-03 21:09:53 +0530

    added buttons

---
+++ app/src/main/java/com/example/mindspace/NotePage.java
@@ -0,0 +1,20 @@
+package com.example.mindspace;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NotePage extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        
+        setContentView(R.layout.notes_page);
+
+    }
+
+
+}
